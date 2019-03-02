962d7f15e51658caca97df8d65f3426aaa6a8245
github username <github email address>
2019-03-02 16:10:49 -0500

    this tutorial doesn't work

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from 'components/App';
+import GlobalStyles from '/styles/globals';
+import ServiceWorker from '/serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const renderApp = () => {
+  ReactDOM.render(
+    <div>
+      <GlobalStyles />
+      <App />
+    </div>
+  )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+renderApp();
+
+ServiceWorker();
